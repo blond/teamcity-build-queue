07be5adfba15386c6456be255f34fcb0734ef1a7
blond <andrewblond@yandex.com>
2016-06-03 18:37:23 +0300

    test(debug): fix tests for debug using

---
+++ test/load-json.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const test = require('ava');
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+
+test.beforeEach(t => {
+    t.context.gotStub = sinon.stub().resolves({ body: { res: 'bla' } });
+
+    t.context.load = proxyquire('../lib/load-json', {
+        got: t.context.gotStub
+    });
+});
+
+test('should return json', async t => {
+    const info = await t.context.load('url');
+
+    t.deepEqual(info, { res: 'bla' });
+});
+
+test('should throw error', t => {
+    t.context.gotStub.rejects('err');
+
+    t.throws(t.context.load('url'), 'err');
+});
